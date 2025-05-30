061a6e33df20508f36ef56b5372c03125544e0f2
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-30 18:11:46 +0530

    Add files via upload

---
+++ frontend/src/components/ProblemList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function ProblemList() {
+  const [problems, setProblems] = useState([]);
+
+  useEffect(() => {
+    const fetchProblems = async () => {
+      const res = await axios.get("http://localhost:5000/api/problems");
+      setProblems(res.data);
+    };
+    fetchProblems();
+  }, []);
+
+  return (
+    <div className="problem-list">
+      {problems.map((p) => (
+        <div key={p._id} className="problem-card">
+          <h3>{p.title} ({p.platform})</h3>
+          <p><strong>Topic:</strong> {p.topic}</p>
+          <pre>{p.code}</pre>
+          <p>{p.notes}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProblemList;

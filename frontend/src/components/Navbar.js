5d0f793bd9c1b1dba4f7bc1cef170dd6f7799389
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 15:07:51 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,28 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const NavigationBar = () => (
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="/">Coding Journal</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/about">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/contact">
+            <Nav.Link>Contact</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default NavigationBar;

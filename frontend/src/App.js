b43d96131212e57ba862392ebecd3047d8419585
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 16:02:21 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import { Container } from 'react-bootstrap';
-import NavbarComponent from './components/Navbar';
-import ProblemForm from './components/ProblemForm';
-import ProblemList from './components/ProblemList';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProblemForm from "./components/ProblemForm";
+import ProblemList from "./components/ProblemList";
 
 function App() {
-  const [problems, setProblems] = useState([]);
-
-  const addProblem = (problem) => {
-    setProblems([...problems, { ...problem, id: Date.now() }]);
-  };
-
-  const deleteProblem = (id) => {
-    setProblems(problems.filter((p) => p.id !== id));
-  };
 
   return (
-    <>
-      <NavbarComponent />
-      <Container className="my-4">
-        <ProblemForm onAddProblem={addProblem} />
-        <hr />
-        <ProblemList problems={problems} onDeleteProblem={deleteProblem} />
-      </Container>
-    </>
+    <Router>
+      <Header onLogout={handleLogout} />
+      <div className="container my-4">
+        <Routes>
+          <Route
+            path="/"
+            element={<ProblemList />}
+          />
+          <Route
+            path="/add-problem"
+            element={<ProblemForm />}
+          />
+          {/* You can add more routes here, like /profile */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

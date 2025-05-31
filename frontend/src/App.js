efb66b18626228f316bf1e25da13658b75fd4614
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 15:10:11 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from './components/Navbar';
 import Header from "./components/Header";
 import ProblemForm from "./components/ProblemForm";
 import ProblemList from "./components/ProblemList";

5b181d840f95e379c8970e445901216cf9160968
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 13:23:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import ProblemForm from "./components/ProblemForm";
 import ProblemList from "./components/ProblemList";
-import "./App.css";
 
 function App() {
+  const handleLogout = () => {
+    // Add logout logic here if you want
+    alert("Logout clicked");
+  };
+
   return (
-    <div className="App">
-      <h1>Coding Journal</h1>
-      <ProblemForm />
-      <ProblemList />
-    </div>
+    <Router>
+      <Header onLogout={handleLogout} />
+      <div className="container my-4">
+        <Routes>
+          <Route
+            path="/"
+            element={<ProblemList />}
+          />
+          <Route
+            path="/add-problem"
+            element={<ProblemForm />}
+          />
+          {/* You can add more routes here, like /profile */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

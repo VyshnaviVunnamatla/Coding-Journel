9b37a040a661c02c5eeb1b2f81cdd049136a913b
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 15:58:03 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-const Header = ({ onLogout }) => {
+function Header({ onLogout }) {
   return (
-    <Navbar bg="dark" variant="dark" expand="md" className="mb-4">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/">Coding Journal</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Coding Journal</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {/* Add nav links if needed */}
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/add-problem">
+              <Nav.Link>Add Problem</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/profile">
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
           </Nav>
-          <Button variant="outline-warning" onClick={onLogout}>
-            Logout
-          </Button>
+          <Nav>
+            <Button variant="outline-light" onClick={onLogout}>
+              Logout
+            </Button>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-};
+}
 
 export default Header;

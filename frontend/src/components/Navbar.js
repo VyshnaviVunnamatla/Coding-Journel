06c182a14ca636559b1ff8eac62ba78f52623ef7
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-05-31 15:46:29 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,28 +1,15 @@
-// src/components/Navbar.js
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
-const NavigationBar = () => (
-  <Navbar bg="dark" variant="dark" expand="lg">
-    <Container>
-      <Navbar.Brand href="/">Coding Journal</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/about">
-            <Nav.Link>About</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-            <Nav.Link>Contact</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+function NavbarComponent() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Coding Journal</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
 
-export default NavigationBar;
+export default NavbarComponent;
+
